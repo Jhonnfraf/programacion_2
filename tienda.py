6317e0d71f2f8046c15f7d25f1e73b74933a09ea
Jhonnfraf <147097991+Jhonnfraf@users.noreply.github.com>
2023-11-24 12:31:42 -0500

    Update tienda.py

--- tienda.py
+++ tienda.py
@@ -35,7 +35,36 @@ class Tienda:
            print(self.producto4)   
 
     def realizar_venta(self,producto,cantidad):
-          if self.producto1.nombre==producto:
-                self.producto1.cant_bodega=self.producto1.cant_bodega-cantidad
-                self.producto1.cant_vendida+=cantidad
-                venta=self.producto1.v_unitario*cantidad
+        if self.producto1.nombre==producto:
+            self.producto1.cant_bodega=self.producto1.cant_bodega-cantidad
+            self.producto1.cant_vendida+=cantidad
+            venta=self.producto1.v_unitario*cantidad
+        elif self.producto2.nombre==producto:
+             self.producto2.cant_bodega=self.producto1.cant_bodega-cantidad
+             venta=self.producto2.v_unitario*cantidad
+             self.producto2.cant_vendida+=cantidad
+        elif self.producto3.nombre==producto:
+             self.producto3.cant_bodega=self.producto3.cant_bodega-cantidad
+             self.producto3.cant_vendida+=cantidad
+             venta=self.producto3.v_unitario*cantidad
+        elif self.producto4.nombre==producto:
+             self.producto4.cant_bodega=self.producto4.cant_bodega-cantidad
+             self.producto4.cant_vendida+=cantidad
+             venta=self.producto4.v_unitario*cantidad
+        Tienda.dinero_caja+=venta
+        
+    def productoMasVendido(self):
+        if self.producto1.cant_vendida>may:
+              may=self.producto1.cant_vendida
+              productomay=self.producto1.nombre
+        elif self.producto2.cant_vendida>may:
+              may=self.producto2.cant_vendida
+              productomay=self.producto2.nombre
+        elif self.producto3.cant_vendida>may:
+              may=self.producto3.cant_vendida
+              productomay=self.producto3.nombre
+        elif self.producto4.cant_vendida>may:
+              may=self.producto4.cant_vendida
+              productomay=self.producto4.nombre
+        
+        print(f"Producto: {productomay}\nCantidad{may}")
